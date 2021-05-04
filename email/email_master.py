f4984b7804764632e4d68e47cb009036238c27a4
isaiah-gordon <isaiah.gordon.developer@gmail.com>
2021-05-04 19:06:48 -0300

    Added module for email report implementation. The module will modify an existing HTML email template with BeautifulSoup4 before sending it via SMTP.

---
+++ email/email_master.py
@@ -0,0 +1,54 @@
+import smtplib, ssl
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from bs4 import BeautifulSoup
+import os
+
+port = 465  # For SSL
+system_email = os.environ.get("email_address")
+password = os.environ.get("email_password")
+
+
+def modify_template(html_string, mod_dict):
+
+    soup = BeautifulSoup(html_string, 'html.parser')
+
+    for modification in mod_dict['text']:
+        print(modification)
+        text = soup.find(id=modification)
+        text.string.replace_with(mod_dict['text'][modification])
+
+    for modification in mod_dict['images']:
+        image = soup.find(id=modification)
+        image['src'] = mod_dict['images'][modification]
+
+    return soup
+
+
+def send_email(receiver_email, subject, template, data_dict):
+
+    message = MIMEMultipart("alternative")
+    message["Subject"] = subject
+    message["From"] = system_email
+    message["To"] = receiver_email
+
+    file = open(template, "r", encoding="utf-8")
+    html = file.read()
+    file.close()
+
+    custom_html = modify_template(html, data_dict)
+
+    # Turn these into plain/html MIMEText objects
+    part1 = MIMEText(custom_html, "html", "utf-8")
+
+    # Add HTML/plain-text parts to MIMEMultipart message
+    # The email client will try to render the last part first
+    message.attach(part1)
+
+    # Create a secure SSL context
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+        server.login(system_email, password)
+
+        server.sendmail(system_email, receiver_email, message.as_string())
